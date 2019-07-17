d75ca83602d8d80b3e26e4de6934edc401cf2bc5
Shea Hunter <shunter@nerdery.com>
2019-07-17 10:20:59 -0700

    initial commit

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    public AudioClip music;
+
+    private AudioSource m_AudioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_AudioSource = GetComponent<AudioSource>();
+
+        if (music)
+        {
+            m_AudioSource.clip = music;
+            m_AudioSource.loop = true;
+            m_AudioSource.Play();
+        }
+    }
+
+}
---
+++ Assets/Scripts/SfxManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SfxManager : MonoBehaviour
+{
+	public AudioClip ambiance;
+
+	private AudioSource m_AudioSource;
+
+	// Use this for initialization
+	void Start()
+    {
+		m_AudioSource = GetComponent<AudioSource>();
+
+		if (ambiance)
+		{
+            m_AudioSource.clip = ambiance;
+            m_AudioSource.loop = true;
+            m_AudioSource.Play();
+		}
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
