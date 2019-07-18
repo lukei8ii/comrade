72fe1090bfd1b6612107525b015dc5dd13f48a24
Shea Hunter <shunter@nerdery.com>
2019-07-17 18:34:21 -0700

    added sfx and idle animations

--- Assets/Scripts/SfxManager.cs
+++ Assets/Scripts/SfxManager.cs
@@ -3,7 +3,24 @@ using System.Collections;
 
 public class SfxManager : MonoBehaviour
 {
-	public AudioClip ambiance;
+    public enum Clip {
+        Burp,
+        Drink,
+        GlassBreak,
+        Hiccup,
+        PassOut,
+        PotatoHit,
+        Slap
+    };
+
+    public AudioClip ambiance;
+    public AudioClip[] burps;
+    public AudioClip[] drinks;
+    public AudioClip[] glassBreaks;
+    public AudioClip[] hiccups;
+    public AudioClip passOut;
+    public AudioClip[] potatoHits;
+    public AudioClip[] slaps;
 
 	private AudioSource m_AudioSource;
 
@@ -20,9 +37,38 @@ public class SfxManager : MonoBehaviour
 		}
 	}
 
-    // Update is called once per frame
-    void Update()
+    public void Play(Clip clip)
     {
+        switch (clip)
+        {
+            case Clip.Burp:
+                PlayRandom(burps);
+                break;
+            case Clip.Drink:
+                PlayRandom(drinks);
+                break;
+            case Clip.GlassBreak:
+                PlayRandom(glassBreaks);
+                break;
+            case Clip.Hiccup:
+                PlayRandom(hiccups);
+                break;
+            case Clip.PassOut:
+                m_AudioSource.PlayOneShot(passOut);
+                break;
+            case Clip.PotatoHit:
+                PlayRandom(potatoHits);
+                break;
+            case Clip.Slap:
+                PlayRandom(slaps);
+                break;
+            default:
+                break;
+        }
+    }
 
+    void PlayRandom(AudioClip[] clips)
+    {
+        m_AudioSource.PlayOneShot(clips[Random.Range(0, clips.Length)]);
     }
 }
