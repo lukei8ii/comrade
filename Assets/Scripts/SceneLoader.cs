8e25a69241af586f87a083cb11bd642ba5e1bff1
Shea Hunter <shunter@nerdery.com>
2019-07-22 00:16:09 -0700

    added credits

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -5,10 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
+    public int nextScene = -1;
+    public float changeSceneAfter = 5f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (nextScene > -1)
+        {
+            LoadSceneDelayed(nextScene, changeSceneAfter);
+        }
     }
 
     // Update is called once per frame
@@ -20,5 +26,16 @@ public class SceneLoader : MonoBehaviour
     public void LoadScene(int sceneIndex)
     {
         SceneManager.LoadScene(sceneIndex);
+    }
+
+    public void LoadSceneDelayed(int sceneIndex, float delay)
+    {
+        StartCoroutine(DelayScene(sceneIndex, delay));
+    }
+
+    IEnumerator DelayScene(int sceneIndex, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        LoadScene(sceneIndex);
     }
 }
